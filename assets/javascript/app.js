c6178f3b6fc0fdb1186be10371a9975b68a91f7f
Lindsey Fischer <lindsfisch@gmail.com>
2017-05-10 20:58:01 -0500

    updated js/timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,49 +4,57 @@ var options = [
 		question: "Pupusas, handmade thick stuffed corn tortillas, are a traditional dish from what country?", 
 		choice: ["Ethiopia", "El Salvadore", "Peru", "Guatamala"],
 		answer: 1,
-		shown: false
+		shown: false,
+		photo: "assets/images/harvey.jpg"
 	 },
 	 {
 	 	question: "What popular soda beverage was originally developed as a mixer for whiskey?", 
 		choice: ["Mountain Dew", "Sprite", "7-UP", "Coke"],
 		answer: 0,
-		shown: false
+		shown: false,
+		photo: "assets/images/harvey.jpg"
 	 }, 
 	 {
 	 	question: "Kopi luwak is a very expensive type of what?", 
 		choice: ["Spice", "Caviar", "Coffee", "Rice variety" ],
 		answer: 2,
-		shown: false
+		shown: false, 
+		photo: "assets/images/harvey.jpg"
 	}, 
 	{
 		question: "Which is not an ingredient in a Harvey Wallbanger cocktail?", 
 		choice: ["Orange Juice", "Vodka", "Sour Mix", "Galliano" ],
 		answer: 2,
-		shown: false
+		shown: false,
+		photo: "assets/images/harvey.jpg"
 	}, 
 	{
 		question: "How many items are there in a Bakers' Dozen?", 
 		choice: ["12", "6", "24", "13" ],
 		answer: 3,
-		shown: false
+		shown: false,
+		photo: "assets/images/harvey.jpg"
 	}, 
 	{
 		question: "What is the most widely eaten fish in the world?", 
 		choice: ["Tilapia", "Hering", "Sardine", "Tuna" ],
 		answer: 1,
-		shown: false
+		shown: false,
+		photo: "assets/images/harvey.jpg"
 	}, 
 	{
 		question: "Which fruit does not ripen once it has been picked?", 
 		choice: ["Banana", "Lemon", "Mango", "Apple" ],
 		answer: 1,
-		shown: false
+		shown: false,
+		photo: "assets/images/harvey.jpg"
 	}, 
 	{
 		question: "Which fruit contains the most protein per 100 calories?", 
 		choice: ["Guava", "Avocado", "Banana", "Blackberries" ],
 		answer: 0,
-		shown: false
+		shown: false,
+		photo: "assets/images/harvey.jpg"
 	}];
 
 var correctCount = 0;
@@ -56,27 +64,35 @@ var timer = 20;
 var intervalId;
 var userGuess ="";
 var running = false;
+var qCount = 0;
+var pick;
+
+console.log(options.length);
 
 //click start button to start game
 $("#start").on("click", function () {
 		$("#start").hide();
 		displayQuestion();
+		runTimer();
 	})
 //timer start
 function runTimer(){
 	if (!running) {
-	intervalID = setInterval(decrement, 1000); 
+	intervalId = setInterval(decrement, 1000); 
 	running = true;
 	}
 }
 //timer countdown
 function decrement() {
-	timer --;
 	$("#timeleft").html("<h3>Time remaining: " + timer + "</h3>");
+	timer --;
+
 	//stop timer if reach 0
 	if (timer === 0) {
 		unanswerCount++;
-		checkOver();
+		stop();
+		$("#answerblock").text("Time is up! The correct answer is: " + pick.choice[pick.answer]);
+		hidepicture();
 	}	
 }
 
@@ -89,12 +105,13 @@ function stop() {
 //display question and loop though and display possible answers
 function displayQuestion() {
 	//generate random index in array
-	var pick = options[Math.floor(Math.random()*options.length)]
-	console.log(pick);
+	pick = options[Math.floor(Math.random()*options.length)]
+	
 	if (pick.shown) {
 		//recursive to continue to generate new index until one is chosen that has not shown in this game yet
 		displayQuestion();
 	} else {
+		console.log(pick);
 		//iterate through answer array and display
 		$("#questionblock").html("<h2>" + pick.question + "</h2>");
 		for(var i = 0; i < pick.choice.length; i++) {
@@ -113,28 +130,45 @@ function displayQuestion() {
 $(".answerchoice").on("click", function () {
 	//grab array position from userGuess
 	userGuess = parseInt($(this).attr("data-guessvalue"));
+
 	//correct guess or wrong guess outcomes
 	if (userGuess === pick.answer) {
-		timer=20;
+		stop();
 		correctCount++;
-		checkOver();
+		userGuess="";
+		$("#answerblock").text("Correct!");
+		hidepicture();
+
 	} else {
-		timer=20;
+		stop();
 		wrongCount++;
-		checkOver();
+		userGuess="";
+		$("#answerblock").text("Wrong! The correct answer is: " + pick.choice[pick.answer]);
+		hidepicture();
 	}
 })
 
 }
 
+function hidepicture () {
+	$("#answerblock").append("<img src=" + pick.photo + ">");
+	var hidpic = setTimeout(function() {
+		$("#answerblock").empty();
+		timer= 20;
+		displayQuestion();
+		runTimer();
+	}, 5000);
+}
 
 function checkOver() {
-	if (unanswerCount + correctCount + wrongCount === options.length) {
-		
+	if (wrongCount + correctCount + unanswerCount === options.length) {
+		stop();
+		$("#questionblock").empty();
+		$(".answerchoice").remove();		
 
 	} else {
 		$(".answerchoice").remove();
-		displayQuestion();
+
 	}
 
 }
