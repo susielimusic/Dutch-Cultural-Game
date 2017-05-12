9e38f1e57350b72b54855bfff61ee142cc73a45d
Lindsey Fischer <lindsfisch@gmail.com>
2017-05-12 14:06:52 -0500

    completed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,19 +4,19 @@ var options = [
 		question: "Pupusas, handmade thick stuffed corn tortillas, are a traditional dish from what country?", 
 		choice: ["Ethiopia", "El Salvadore", "Peru", "Guatamala"],
 		answer: 1,
-		photo: "assets/images/harvey.jpg"
+		photo: "assets/images/pupusas.jpg"
 	 },
 	 {
 	 	question: "What popular soda beverage was originally developed as a mixer for whiskey?", 
 		choice: ["Mountain Dew", "Sprite", "7-UP", "Coke"],
 		answer: 0,
-		photo: "assets/images/harvey.jpg"
+		photo: "assets/images/mtdew.gif"
 	 }, 
 	 {
 	 	question: "Kopi luwak is a very expensive type of what?", 
 		choice: ["Spice", "Caviar", "Coffee", "Rice variety" ],
 		answer: 2,
-		photo: "assets/images/harvey.jpg"
+		photo: "assets/images/coffee.gif"
 	}, 
 	{
 		question: "Which is not an ingredient in a Harvey Wallbanger cocktail?", 
@@ -28,25 +28,25 @@ var options = [
 		question: "How many items are there in a Bakers' Dozen?", 
 		choice: ["12", "6", "24", "13" ],
 		answer: 3,
-		photo: "assets/images/harvey.jpg"
+		photo: "assets/images/dozen.jpg"
 	}, 
 	{
 		question: "What is the most widely eaten fish in the world?", 
-		choice: ["Tilapia", "Hering", "Sardine", "Tuna" ],
+		choice: ["Tilapia", "Herring", "Sardine", "Tuna" ],
 		answer: 1,
-		photo: "assets/images/harvey.jpg"
+		photo: "assets/images/herring.jpg"
 	}, 
 	{
 		question: "Which fruit does not ripen once it has been picked?", 
 		choice: ["Banana", "Lemon", "Mango", "Apple" ],
 		answer: 1,
-		photo: "assets/images/harvey.jpg"
+		photo: "assets/images/lemon.gif"
 	}, 
 	{
 		question: "Which fruit contains the most protein per 100 calories?", 
 		choice: ["Guava", "Avocado", "Banana", "Blackberries" ],
 		answer: 0,
-		photo: "assets/images/harvey.jpg"
+		photo: "assets/images/guava.gif"
 	}];
 
 var correctCount = 0;
